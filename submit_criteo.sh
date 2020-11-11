6d654f6c11a60e012adec16ef0a33ce2c39494b3
eddojansen <eddojansen@gmail.com>
2020-11-11 18:15:50 +0000

    Fixed floating point calculation Fixed tuning settings

--- submit_criteo.sh
+++ submit_criteo.sh
@@ -1,31 +1,31 @@
 set -eux
 
-CMD_PARAM="--master $MASTER \
-    --jars $JARS \
+RESOURCE_GPU_AMT=$(echo "scale=3; ${NUM_EXECUTORS} / ${TOTAL_CORES}" | bc)
+#RESOURCE_GPU_AMT=$(echo "scale=3; ${NUM_EXECUTORS} / ${NUM_EXECUTOR_CORES}" | bc)
+
+TRANSENDDAY=$((${ENDDAY}-1))
+
+if [ $ENABLE_GPU = "false" ]
+  then CMD_PARAM="--master ${MASTER} \
+    --jars ${JARS} \
     --driver-memory ${DRIVER_MEMORY} \
-    --executor-cores $NUM_EXECUTOR_CORES \
+    --executor-cores ${NUM_EXECUTOR_CORES} \
     --executor-memory ${EXECUTOR_MEMORY} \
-    --conf spark.executor.resource.gpu.amount=${RESOURCE_GPU_AMOUNT} \
-    --conf spark.cores.max=$TOTAL_CORES \
-    --conf spark.files.maxPartitionBytes=1342177280 \
-    --conf spark.task.cpus=${NUM_EXECUTOR_CORES} \
-    --conf spark.task.resource.gpu.amount=${RESOURCE_GPU_AMT} \
-    --conf spark.sql.extensions=${SQL_EXTENSIONS} \
-    --conf spark.plugins=${SPARK_PLUGINS} \
-    --conf spark.rapids.sql.reader.batchSizeRows=4000000 \
-    --conf spark.rapids.memory.pinnedPool.size=16g \
-    --conf spark.sql.autoBroadcastJoinThreshold=1GB \
-    --conf spark.rapids.sql.incompatibleOps.enabled=${INCOMPATIBLE_OPS} \
-    --conf spark.sql.files.maxPartitionBytes=1G \
+    --conf spark.sql.shuffle.partitions=$(( ${NUM_EXECUTORS} * 2 )) \
+    --conf spark.task.cpus=2 \
+    --conf spark.cores.max=${TOTAL_CORES} \
+    --conf spark.sql.autoBroadcastJoinThreshold=1G \
     --conf spark.driver.maxResultSize=2G \
+    --conf spark.sql.files.maxPartitionBytes=1G \
+    --conf spark.executor.heartbeatInterval=300s \
+    --conf spark.storage.blockManagerSlaveTimeoutMs=3600s \
+    --conf spark.sql.files.maxPartitionBytes=${MAXPARTITIONBYTES} \
     --conf spark.locality.wait=0s \
-    --conf spark.executor.extraClassPath=${XGBOOST_JAR}:${XGBOOST_SPARK_JAR}:${SPARK_RAPIDS_PLUGIN_JAR}:${SPARK_CUDF_JAR} \
-    --conf spark.driver.extraClassPath=${XGBOOST_JAR}:${XGBOOST_SPARK_JAR}:${SPARK_RAPIDS_PLUGIN_JAR}:${SPARK_CUDF_JAR} \
-    --conf spark.network.timeout=1800s \
-    --conf spark.hadoop.fs.s3a.access.key=$S3A_CREDS_USR \
-    --conf spark.hadoop.fs.s3a.secret.key=$S3A_CREDS_PSW \
+    --conf spark.network.timeout=3600s \
+    --conf spark.hadoop.fs.s3a.access.key=${S3A_CREDS_USR} \
+    --conf spark.hadoop.fs.s3a.secret.key=${S3A_CREDS_PSW} \
     --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
-    --conf spark.hadoop.fs.s3a.endpoint=$S3_ENDPOINT \
+    --conf spark.hadoop.fs.s3a.endpoint=${S3_ENDPOINT} \
     --conf spark.hadoop.fs.s3a.path.style.access=true \
     --conf spark.hadoop.fs.s3a.experimental.input.fadvise=sequential \
     --conf spark.hadoop.fs.s3a.connection.maximum=1000\
@@ -34,38 +34,79 @@ CMD_PARAM="--master $MASTER \
     --conf spark.sql.parquet.mergeSchema=false \
     --conf spark.sql.parquet.filterPushdown=true \
     --conf spark.sql.hive.metastorePartitionPruning=true \
-    --conf spark.hadoop.fs.s3a.connection.ssl.enabled=true"    
-    
-/opt/spark/bin/spark-submit $CMD_PARAM \
-    	--conf spark.sql.shuffle.partitions=600 \
-    	--conf spark.executor.extraJavaOptions="${EXTRA_JAVA_OPTIONS}" \
-    	$SCRIPT --mode generate_models \
-    	--input_folder $INPUT_PATH \
-    	--frequency_limit $FREQUENCY_LIMIT \
-        --debug_mode \
-    	--days ${STARTDAY}-${ENDDAY} \
-    	--model_folder $OUTPUT_PATH/models \
-	--write_mode overwrite --low_mem &&
+    --conf spark.hadoop.fs.s3a.connection.ssl.enabled=true" &&
+    TEST="cpu_test" && 
+  echo "Running CPU mode"
 
-/opt/spark/bin/spark-submit $CMD_PARAM \
-	--conf spark.sql.shuffle.partitions=600 \
-        --conf spark.executor.extraJavaOptions="${EXTRA_JAVA_OPTIONS}" \
-	$SCRIPT --mode transform \
-        --input_folder $INPUT_PATH \
+  else CMD_PARAM="--master ${MASTER} \
+    --jars ${JARS} \
+    --driver-memory ${DRIVER_MEMORY} \
+    --executor-cores ${NUM_EXECUTOR_CORES} \
+    --executor-memory ${EXECUTOR_MEMORY} \
+    --conf spark.sql.shuffle.partitions=$(( ${NUM_EXECUTORS} * ${CONCURRENTGPU} )) \
+    --conf spark.task.cpus=1 \
+    --conf spark.executor.resource.gpu.amount=1 \
+    --conf spark.executor.extraJavaOptions="-Dai.rapids.cudf.prefer-pinned=true" \
+    --conf spark.cores.max=${TOTAL_CORES} \
+    --conf spark.task.resource.gpu.amount=${RESOURCE_GPU_AMT} \
+    --conf spark.sql.extensions=com.nvidia.spark.rapids.SQLExecPlugin \
+    --conf spark.plugins=com.nvidia.spark.SQLPlugin \
+    --conf spark.rapids.sql.incompatibleOps.enabled=true \
+    --conf spark.rapids.sql.concurrentGpuTasks=${CONCURRENTGPU} \
+    --conf spark.rapids.sql.batchSizeRows=4000000 \
+    --conf spark.rapids.sql.reader.batchSizeRows=4000000 \
+    --conf spark.rapids.memory.pinnedPool.size=4G \
+    --conf spark.sql.autoBroadcastJoinThreshold=1GB \
+    --conf spark.driver.maxResultSize=2G \
+    --conf spark.sql.files.maxPartitionBytes=1G \
+    --conf spark.executor.heartbeatInterval=300s \
+    --conf spark.storage.blockManagerSlaveTimeoutMs=3600s \
+    --conf spark.sql.files.maxPartitionBytes=${MAXPARTITIONBYTES} \
+    --conf spark.locality.wait=0s \
+    --conf spark.network.timeout=1800s \
+    --conf spark.executor.extraClassPath=${XGBOOST_JAR}:${XGBOOST_SPARK_JAR}:${SPARK_RAPIDS_PLUGIN_JAR}:${SPARK_CUDF_JAR} \
+    --conf spark.driver.extraClassPath=${XGBOOST_JAR}:${XGBOOST_SPARK_JAR}:${SPARK_RAPIDS_PLUGIN_JAR}:${SPARK_CUDF_JAR} \
+    --conf spark.hadoop.fs.s3a.access.key=${S3A_CREDS_USR} \
+    --conf spark.hadoop.fs.s3a.secret.key=${S3A_CREDS_PSW} \
+    --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
+    --conf spark.hadoop.fs.s3a.endpoint=${S3_ENDPOINT} \
+    --conf spark.hadoop.fs.s3a.path.style.access=true \
+    --conf spark.hadoop.fs.s3a.experimental.input.fadvise=sequential \
+    --conf spark.hadoop.fs.s3a.connection.maximum=1000\
+    --conf spark.hadoop.fs.s3a.threads.core=1000\
+    --conf spark.hadoop.parquet.enable.summary-metadata=false \
+    --conf spark.sql.parquet.mergeSchema=false \
+    --conf spark.sql.parquet.filterPushdown=true \
+    --conf spark.sql.hive.metastorePartitionPruning=true \
+    --conf spark.hadoop.fs.s3a.connection.ssl.enabled=true" && 
+  TEST="gpu_test" &&
+  echo "Running GPU mode"
+fi
+
+/opt/spark/bin/spark-submit ${CMD_PARAM} \
+	${SCRIPT} --mode generate_models \
+        --input_folder ${INPUT_PATH} \
+        --frequency_limit ${FREQUENCY_LIMIT} \
         --debug_mode \
         --days ${STARTDAY}-${ENDDAY} \
-        --output_folder $OUTPUT_PATH/train \
-        --model_folder $OUTPUT_PATH/models \
+        --model_folder ${OUTPUT_PATH}/models \
         --write_mode overwrite --low_mem &&
 
-/opt/spark/bin/spark-submit $CMD_PARAM \
-        --conf spark.sql.shuffle.partitions=30 \
-        --conf spark.executor.extraJavaOptions="${EXTRA_JAVA_OPTIONS}" \
-	$SCRIPT --mode transform \
-        --input_folder $INPUT_PATH \
+/opt/spark/bin/spark-submit ${CMD_PARAM} \
+	${SCRIPT} --mode transform \
+        --input_folder ${INPUT_PATH} \
         --debug_mode \
-        --days ${STARTDAY}-${ENDDAY} \
-        --output_folder $OUTPUT_PATH/gpu_test \
+        --days ${STARTDAY}-${TRANSENDDAY} \
+        --output_folder ${OUTPUT_PATH}/train \
+        --model_folder ${OUTPUT_PATH}/models \
+        --write_mode overwrite --low_mem &&
+
+/opt/spark/bin/spark-submit ${CMD_PARAM} \
+	${SCRIPT} --mode transform \
+        --input_folder ${INPUT_PATH} \
+        --debug_mode \
+        --days ${ENDDAY}-${ENDDAY} \
+        --output_folder ${OUTPUT_PATH}/${TEST} \
         --output_ordering input \
-        --model_folder $OUTPUT_PATH/models \
-        --write_mode overwrite --low_mem 
+        --model_folder ${OUTPUT_PATH}/models \
+        --write_mode overwrite --low_mem
