75b25c1b76a146d5a42e293473392705fe0d1b57
eddojansen <eddojansen@gmail.com>
2020-11-19 00:04:25 +0000

    Adjusted partition number and bytesize calculations

--- submit_criteo.sh
+++ submit_criteo.sh
@@ -1,7 +1,6 @@
 set -eux
 
 RESOURCE_GPU_AMT=$(echo "scale=3; ${NUM_EXECUTORS} / ${TOTAL_CORES}" | bc)
-#RESOURCE_GPU_AMT=$(echo "scale=3; ${NUM_EXECUTORS} / ${NUM_EXECUTOR_CORES}" | bc)
 
 TRANSENDDAY=$((${ENDDAY}-1))
 
@@ -43,23 +42,23 @@ if [ $ENABLE_GPU = "false" ]
     --driver-memory ${DRIVER_MEMORY} \
     --executor-cores ${NUM_EXECUTOR_CORES} \
     --executor-memory ${EXECUTOR_MEMORY} \
-    --conf spark.sql.shuffle.partitions=6400 \
+    --conf spark.sql.files.maxPartitionBytes=${MAXPARTITIONBYTES} \
+    --conf spark.sql.shuffle.partitions=${SHUFFLE_PARTITIONS} \
+    --conf spark.shuffle.consolidateFiles=true \
     --conf spark.task.cpus=1 \
     --conf spark.executor.resource.gpu.amount=1 \
     --conf spark.executor.extraJavaOptions="-Dai.rapids.cudf.prefer-pinned=true" \
     --conf spark.cores.max=${TOTAL_CORES} \
     --conf spark.sql.autoBroadcastJoinThreshold=1G \
-    --conf spark.driver.maxResultSize=2G \
     --conf spark.sql.files.maxPartitionBytes=1G \
     --conf spark.task.resource.gpu.amount=${RESOURCE_GPU_AMT} \
     --conf spark.sql.extensions=com.nvidia.spark.rapids.SQLExecPlugin \
     --conf spark.plugins=com.nvidia.spark.SQLPlugin \
     --conf spark.rapids.sql.incompatibleOps.enabled=true \
     --conf spark.rapids.sql.concurrentGpuTasks=${CONCURRENTGPU} \
-    --conf spark.rapids.memory.pinnedPool.size=8G \
+    --conf spark.rapids.memory.pinnedPool.size=${PINNED_POOL_SIZE} \
     --conf spark.executor.heartbeatInterval=300s \
     --conf spark.storage.blockManagerSlaveTimeoutMs=3600s \
-    --conf spark.sql.files.maxPartitionBytes=128M \
     --conf spark.locality.wait=0s \
     --conf spark.network.timeout=1800s \
     --conf spark.executor.extraClassPath=${XGBOOST_JAR}:${XGBOOST_SPARK_JAR}:${SPARK_RAPIDS_PLUGIN_JAR}:${SPARK_CUDF_JAR} \
@@ -70,8 +69,8 @@ if [ $ENABLE_GPU = "false" ]
     --conf spark.hadoop.fs.s3a.endpoint=${S3_ENDPOINT} \
     --conf spark.hadoop.fs.s3a.path.style.access=true \
     --conf spark.hadoop.fs.s3a.experimental.input.fadvise=sequential \
-    --conf spark.hadoop.fs.s3a.connection.maximum=1000 \
-    --conf spark.hadoop.fs.s3a.threads.core=1000 \
+    --conf spark.hadoop.fs.s3a.connection.maximum=$(( ${TOTAL_CORES} * 2 )) \
+    --conf spark.hadoop.fs.s3a.threads.core=${TOTAL_CORES} \
     --conf spark.hadoop.parquet.enable.summary-metadata=false \
     --conf spark.sql.parquet.mergeSchema=false \
     --conf spark.sql.parquet.filterPushdown=true \
@@ -87,7 +86,8 @@ fi
         --debug_mode \
         --days ${STARTDAY}-${ENDDAY} \
         --model_folder ${OUTPUT_PATH}/models \
-        --write_mode overwrite --low_mem &&
+        --write_mode overwrite && 
+#        --write_mode overwrite --low_mem &&
 
 /opt/spark/bin/spark-submit ${CMD_PARAM} \
 	${SCRIPT} --mode transform \
@@ -96,7 +96,8 @@ fi
         --days ${STARTDAY}-${TRANSENDDAY} \
         --output_folder ${OUTPUT_PATH}/train \
         --model_folder ${OUTPUT_PATH}/models \
-        --write_mode overwrite --low_mem &&
+        --write_mode overwrite &&
+#        --write_mode overwrite --low_mem &&
 
 /opt/spark/bin/spark-submit ${CMD_PARAM} \
 	${SCRIPT} --mode transform \
@@ -106,4 +107,5 @@ fi
         --output_folder ${OUTPUT_PATH}/${TEST} \
         --output_ordering input \
         --model_folder ${OUTPUT_PATH}/models \
-        --write_mode overwrite --low_mem
+        --write_mode overwrite 
+#        --write_mode overwrite --low_mem
